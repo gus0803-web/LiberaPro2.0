10734329f72f15c39fbdde38e5b34ebc02a7a65d
Gustavo <gus0803@Gustavos-Mac-mini.local>
2026-08-25 21:08:33 -0600

    Hardcode Google Analytics ID G-X3XSQ55Q5F

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,22 +40,18 @@ export default function RootLayout({
     >
       <head>
         {/* Google Analytics Scripts */}
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X3XSQ55Q5F"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X3XSQ55Q5F');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
